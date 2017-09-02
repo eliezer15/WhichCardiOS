a1a80800a03947947c311eb27d132b097913b468
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2017-09-02 17:17:38 -0400

    Project started

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class FixedDimensionsBasic extends Component {
+    render() {
+        return (
+            <ScrollView contentContainerStyle={{paddingTop: 100}} horizontal={true}>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+                <Text>Hi, I'm Eli </Text>
+            </ScrollView>
+        );
+    }
+}
